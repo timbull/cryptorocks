44a1ca9a0f0b41039dedc7fddc0b26253e42e360
andy <ggez44@gmail.com>
2018-02-09 17:48:30 -0800

    ok

--- src/js/app.js
+++ src/js/app.js
@@ -24,30 +24,30 @@ App = {
   },
 
   initWeb3: function() {
-// Is there an injected web3 instance?
-if (typeof web3 !== 'undefined') {
-  App.web3Provider = web3.currentProvider;
-} else {
-  // If no injected web3 instance is detected, fall back to Ganache
-  App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-}
-web3 = new Web3(App.web3Provider);
-
+    // Is there an injected web3 instance?
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+    } else {
+      // If no injected web3 instance is detected, fall back to Ganache
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    }
+    web3 = new Web3(App.web3Provider);
+    
     return App.initContract();
   },
 
   initContract: function() {
-$.getJSON('Adoption.json', function(data) {
-  // Get the necessary contract artifact file and instantiate it with truffle-contract
-  var AdoptionArtifact = data;
-  App.contracts.Adoption = TruffleContract(AdoptionArtifact);
-
-  // Set the provider for our contract
-  App.contracts.Adoption.setProvider(App.web3Provider);
-
-  // Use our contract to retrieve and mark the adopted pets
-  return App.markAdopted();
-});
+    $.getJSON('Adoption.json', function(data) {
+      // Get the necessary contract artifact file and instantiate it with truffle-contract
+      var AdoptionArtifact = data;
+      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+    
+      // Set the provider for our contract
+      App.contracts.Adoption.setProvider(App.web3Provider);
+    
+      // Use our contract to retrieve and mark the adopted pets
+      return App.markAdopted();
+    });
 
     return App.bindEvents();
   },
@@ -57,22 +57,21 @@ $.getJSON('Adoption.json', function(data) {
   },
 
   markAdopted: function(adopters, account) {
-var adoptionInstance;
+    var adoptionInstance;
 
-App.contracts.Adoption.deployed().then(function(instance) {
-  adoptionInstance = instance;
-
-  return adoptionInstance.getAdopters.call();
-}).then(function(adopters) {
-  for (i = 0; i < adopters.length; i++) {
-    if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
-      $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
-    }
-  }
-}).catch(function(err) {
-  console.log(err.message);
-});
-
+    App.contracts.Adoption.deployed().then(function(instance) {
+      adoptionInstance = instance;
+    
+      return adoptionInstance.getAdopters.call();
+    }).then(function(adopters) {
+      for (i = 0; i < adopters.length; i++) {
+        if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
+          $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
+        }
+      }
+    }).catch(function(err) {
+      console.log(err.message);
+    });
   },
 
   handleAdopt: function(event) {
@@ -80,27 +79,38 @@ App.contracts.Adoption.deployed().then(function(instance) {
 
     var petId = parseInt($(event.target).data('id'));
 
-var adoptionInstance;
+    var adoptionInstance;
+    
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+    
+      var account = accounts[0];
+    
+      App.contracts.Adoption.deployed().then(function(instance) {
+        adoptionInstance = instance;
+    
+        // Execute adopt as a transaction by sending account
+        return adoptionInstance.adopt(petId, {from: account});
+      }).then(function(result) {
+        return App.markAdopted();
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
 
-web3.eth.getAccounts(function(error, accounts) {
-  if (error) {
-    console.log(error);
-  }
+  },
 
-  var account = accounts[0];
+  bindEvents: function() {
+    $(document).on('click', '.pick', App.handleRPS);
+  },
 
-  App.contracts.Adoption.deployed().then(function(instance) {
-    adoptionInstance = instance;
+  handleRPS: function(event) {
+    var choice = $(event.target).data('id');
+    console.log(choice)
 
-    // Execute adopt as a transaction by sending account
-    return adoptionInstance.adopt(petId, {from: account});
-  }).then(function(result) {
-    return App.markAdopted();
-  }).catch(function(err) {
-    console.log(err.message);
-  });
-});
-
+    $('#rps_result').html("you've selected " + choice);
   }
 
 };
